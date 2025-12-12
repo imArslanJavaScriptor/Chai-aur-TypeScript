0e2a028fe35c1140e0a4d10d18d35e274259dc63
Muhammad Arslan <imarslan7061@gmail.com>
2025-12-12 23:20:44 +0500

    upd

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,15 @@ function greet(name: string): string {
     return `Hello, ${name}!`;
 }
 console.log(greet("World"));
+
+
+function findUserAge(user: { name: string; age?: number }): string {
+    if (user.age !== undefined) {
+        return `${user.name} is ${user.age} years old.`;
+    } else {
+        return `Age of ${user.name} is unknown.`;
+    }
+}
+
+console.log(findUserAge({ name: "Alice", age: 30 }));
+console.log(findUserAge({ name: "Bob" }));
